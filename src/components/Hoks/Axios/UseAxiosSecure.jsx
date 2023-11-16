3f7a562f5559c409c0cc372c69e7501ea5e3c263
Your Name <you@example.com>
2023-11-17 00:12:46 +0600

    project is done

--- src/components/Hoks/Axios/UseAxiosSecure.jsx
+++ src/components/Hoks/Axios/UseAxiosSecure.jsx
@@ -1,13 +1,17 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import UseAuth from "../../../Provider/Hoks/UseAuth";
 
  const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000' 
 })
 
 const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const {logOut} = UseAuth();
     axiosSecure.interceptors.request.use(function (config){
         const token = localStorage.getItem('access-token')
-        console.log('request stopped by interceptors',token)
+        // console.log('request stopped by interceptors',token)
         config.headers.authorization = `Bearer ${token}`;
         return config;
     }, function (error) {
@@ -20,9 +24,15 @@ const useAxiosSecure = () => {
         return response;
 
 
-    }, (error)=> {
+    }, async (error)=> {
         const status = error.response.status;
-       console.log('status error in the interceptors', status)
+    //    console.log('status error in the interceptors', status);
+       if(status === 401 || status === 403){
+        await logOut();
+
+         navigate('/login')
+
+       }
         return Promise.reject(error);
     })
        
--- src/components/Layout/Dashboard/Dashboard.jsx
+++ src/components/Layout/Dashboard/Dashboard.jsx
@@ -1,12 +1,13 @@
 import { FaAd, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaSearch, FaShoppingCart, FaUser, FaUsers, FaUtensilSpoon } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import UseCart from "../../Hoks/UseCart";
+import useAdmin from "../../Hoks/Axios/useAdmin";
 
 
 const Dashboard = () => {
     const [cart] = UseCart();
 
-    const isAdmin = true;
+    const [isAdmin] = useAdmin();
     return (
         <div className="flex">
             <div className="w-64 min-h-screen bg-purple-500">
